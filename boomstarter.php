dae1077619f80c328be978583221768018af76fc
Anton Shurashov <sinkler@sinkler.ru>
2013-12-19 03:38:45 +0400

    clean, additional methods

--- boomstarter.php
+++ boomstarter.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Boomstarter
- * 
+ * BoomStarter
+ *
  * Набор методов для работы с API
- * 
+ *
  * @author Drax <dj@boomstarter.ru>
  * @version 1.0
  */
-class Boomstarter
+class BoomStarter
 {
     /**
      * UUID магазина
@@ -15,63 +15,103 @@ class Boomstarter
      * @var string UUID магазина
      */
     private $uuid;
-    
+
     /**
      * Token для доступа к API
      *
      * @var string Token для доступа к API
      */
     private $token;
-	
+
     /**
      * Конструктор класса, объявление переменных
      *
-     * @param uuid $uuid UUID магазина
-     * @param token $token Token магазина
+     * @param string $uuid UUID магазина
+     * @param string $token Token магазина
      */
     public function __construct($uuid, $token)
     {
         $this->uuid = $uuid;
         $this->token = $token;
     }
-	
+
+    public function giftListAll($limit = 250, $offset = 0)
+    {
+        return $this->giftList(null, $limit, $offset);
+    }
+
+    public function giftListPending($limit = 250, $offset = 0)
+    {
+        return $this->giftList('pending', $limit, $offset);
+    }
+
+    public function giftListShipping($limit = 250, $offset = 0)
+    {
+        return $this->giftList('shipping', $limit, $offset);
+    }
+
+    public function giftListDelivered($limit = 250, $offset = 0)
+    {
+        return $this->giftList('delivered', $limit, $offset);
+    }
+
+    public function giftStatusOrder($uuid, $order_id)
+    {
+        return $this->giftStatus('order', $uuid, array('order_id' => $order_id));
+    }
+
+    public function giftStatusSchedule($uuid, $timestamp)
+    {
+        return $this->giftStatus('schedule', $uuid, array('delivery_date' => date('Y-m-d', $timestamp)));
+    }
+
+    public function giftStatusDeliveryState($uuid)
+    {
+        return $this->giftStatus('delivery_state', $uuid, array('delivery_state' => 'delivery'));
+    }
+
     /**
      * Получить список подарков
      *
-     * @param type string Статус подарка (pending, shipping, delivered)
-     * @param param array Набор параметров для обращения к API
+     * @param string $type Статус подарка (pending, shipping, delivered)
+     * @param int $limit
+     * @param int $offset
      * @return array Список подарков
      */
-    public function giftList($type = null, $param = array())
+    protected function giftList($type = null, $limit = 250, $offset = 0)
     {
-        $param['shop_uuid'] = $this->uuid;
-        $param['shop_token'] = $this->token;
-        $url = '/gifts'.($type ? '/'.$type : '').'?'.http_build_query($param);
+        $param = array(
+            'limit' => $limit,
+            'offset' => $offset,
+            'shop_uuid' => $this->uuid,
+            'shop_token' => $this->token,
+        );
+        $url = '/gifts' . ($type ? '/' . $type : '') . '?' . http_build_query($param);
         $data = self::getData($url);
         return $data;
     }
-    
+
     /**
      * Изменить статус подарка
      *
-     * @param type string Статус подарка (pending, shipping, delivered)
-     * @param uuid string UUID подарка
-     * @param param array Набор параметров для обращения к API
+     * @param string $type Статус подарка (order, schedule, delivery_state)
+     * @param string $uuid UUID подарка
+     * @param array $param Набор параметров для обращения к API
      * @return array Ответ API
      */
-    public function giftStatus($type, $uuid, $param = array())
+    protected function giftStatus($type, $uuid, $param = array())
     {
+        $url = '';
+        $put = false;
         switch ($type) {
             case 'order':
-                $url = '/gifts/'.$uuid.'/order';
-                $put = false;
+                $url = '/gifts/' . $uuid . '/order';
                 break;
             case 'schedule':
-                $url = '/gifts/'.$uuid.'/schedule';
-                $put = false;
+                $url = '/gifts/' . $uuid . '/schedule';
                 break;
             case 'delivery_state':
-                $url = '/gifts/'.$uuid.'/delivery_state';
+                $url = '/gifts/' . $uuid . '/delivery_state';
                 $put = true;
                 break;
         }
@@ -82,27 +122,16 @@ class Boomstarter
     }
 
     /**
-     * Показать кнопку "Хочу в подарок"
-     *
-     * @param uuid integer UUID подарка
-     * @return string HTML код кнопки
-     */
-    public function gift($uuid)
-    {
-        return '<a href="#" product-id="'.$uuid.'" boomstarter-button-style="glassy">Хочу в подарок</a><script type="text/javascript" src="//boomstarter.ru/assets/gifts/api/v1.js" async="async"></script>';
-    }
-
-    /**
      * Запрос на получение данных
      *
-     * @param url string Ссылка на API
-     * @param post string POST данные для отправки
-     * @param put boolean Метод запроса
+     * @param string $url Ссылка на API
+     * @param string $post POST данные для отправки
+     * @param boolean $put Метод запроса
      * @return array
      */
     private function getData($url, $post = null, $put = false)
     {
-        $ch = curl_init('https://boomstarter.ru/api/v1.1/partners'.$url);
+        $ch = curl_init('https://boomstarter.ru/api/v1.1/partners' . $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         if ($put) {
@@ -114,6 +143,17 @@ class Boomstarter
         }
         $data = curl_exec($ch);
         curl_close($ch);
-        return json_decode($data,1);
+        return json_decode($data, true);
+    }
+
+    /**
+     * Показать кнопку "Хочу в подарок"
+     *
+     * @param integer $uuid UUID подарка
+     * @return string HTML код кнопки
+     */
+    public function gift($uuid)
+    {
+        return '<a href="#" product-id="' . $uuid . '" boomstarter-button-style="glassy">Хочу в подарок</a><script type="text/javascript" src="//boomstarter.ru/assets/gifts/api/v1.js" async="async"></script>';
     }
 }
